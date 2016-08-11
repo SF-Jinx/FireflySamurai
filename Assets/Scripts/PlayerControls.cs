2a97a6ba4a620230f6ba5cc60c84d6e660393d0b
Jinx <jmm015@lvc.edu>
2016-08-11 12:24:55 -0400

    Beginning to implement attack feature. Dash feature improved.

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -2,10 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 
-
 public class PlayerControls : MonoBehaviour
 {
-
     public string playerNumber;
     public float speed = 7f;
     public float dashSpeed = 7f;
@@ -13,27 +11,37 @@ public class PlayerControls : MonoBehaviour
     public float dashTimeCurrent = .5f;
     public float dashCooldown = 2f;
     public float dashCooldownCurrent = 0f;
+    public float lightCooldown = .1f;
+    public float damageCooldown = 1f;
 
     public AudioClip dashSound;
     public AudioClip attackSound;
 
-    private TrailRenderer Trail;
     private Timers Dash = new Timers(0, 0, 0, 0);
+    private Timers Light = new Timers(0, 0, 0, 0);
+    private Timers Damage = new Timers(0, 0, 0, 0);
     private Vector2 speedVector;
     private Vector2 movement;
     private Vector2 prevMovement;
     private Rigidbody2D rb2d;
     private bool isDashing = false;
+    private string facing = "down";
 
     private bool releasedAttack = true;
     private bool releasedDash = true;
+
+    private bool beingDamaged = false;
+
+    public GameObject TrailPrefab;
+    private GameObject trail;
 
     void Start()
     {
         speedVector = new Vector2(speed, speed);
         rb2d = GetComponent<Rigidbody2D>();
-        Trail = GetComponentInChildren<TrailRenderer>();
         Dash.updateValues(dashTime, dashTimeCurrent, dashCooldown, dashCooldownCurrent);
+        Light.updateValues(0, 0, lightCooldown, 0);
+        Damage.updateValues(0, 0, damageCooldown, 0);
 
         prevMovement = Vector2.zero;
     }
@@ -50,8 +58,16 @@ public class PlayerControls : MonoBehaviour
             inputY * speedVector.y
         );
 
-        handleDashing(inputDash);
-        handleAttack(inputAttack);
+        handleCooldowns();
+
+        if (!beingDamaged)
+        {
+            handleFacing(inputX, inputY);
+            handleDashing(inputDash);
+            handleAttack(inputAttack);
+        }
+
+        handleDamage("", false);
 
         if (Input.GetButtonDown("Submit" + playerNumber))
         {
@@ -64,20 +80,29 @@ public class PlayerControls : MonoBehaviour
         rb2d.velocity = movement;
     }
 
-    private void handleDashing(float inputDash)
+    private void handleCooldowns()
     {
         Dash.updateCooldown(Dash.CurrentCooldownTime - Time.deltaTime);
+        Light.updateCooldown(Light.CurrentCooldownTime - Time.deltaTime);
+        Damage.updateCooldown(Damage.CurrentCooldownTime - Time.deltaTime);
+    }
 
+    private void handleDashing(float inputDash)
+    {
         bool canDash = (Dash.CurrentCooldownTime < 0);
 
         // If you can't dash, you DONE son!
         if (!canDash) return;
 
         if (inputDash == 0) releasedDash = true;
-        if (inputDash == 1 && releasedDash)
+        if (inputDash == 1 && releasedDash && movement.magnitude > 0)
         {
             isDashing = true;
             releasedDash = false;
+
+            trail = (GameObject)Instantiate(TrailPrefab);
+            trail.transform.parent = transform;
+            trail.transform.position = this.gameObject.transform.position;
         }
 
         if (isDashing)
@@ -87,7 +112,6 @@ public class PlayerControls : MonoBehaviour
 
             // Turn on both continuous collision mode and the dash trail
             rb2d.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
-            if (Trail != null) Trail.gameObject.SetActive(true);
 
             // Finished dashing
             if (Dash.CurrentTime < 0)
@@ -95,7 +119,6 @@ public class PlayerControls : MonoBehaviour
                 rb2d.collisionDetectionMode = CollisionDetectionMode2D.Discrete;
 
                 isDashing = false;
-                Trail.probeAnchor = rb2d.transform;
                 prevMovement = Vector2.zero;
 
                 Dash.CurrentTime = Dash.DefaultTime;
@@ -108,22 +131,21 @@ public class PlayerControls : MonoBehaviour
                 {
                     SoundManager.instance.RandomizeSfx(dashSound);
                     prevMovement = movement;
+                    prevMovement.Normalize();
                 }
-                movement = prevMovement * dashSpeed;
+                movement = prevMovement * speed * dashSpeed;
                 isDashing = true;
             }
         }
         else
         {
             // Remove the DASH object so that it is not visible
-            if (Trail != null) Trail.gameObject.SetActive(false);
+            Destroy(trail);
         }
     }
 
     private void handleFacing(float inputX, float inputY)
     {
-        string facing = "down";
-
         // HORIZONTAL MOVEMENT
         if (Mathf.Abs(inputX) > Mathf.Abs(inputY))
         {
@@ -134,36 +156,71 @@ public class PlayerControls : MonoBehaviour
         // VERTICAL MOVEMENT
         else if (Mathf.Abs(inputY) > 0)
         {
-            if (inputY > 0) facing = "down";
+            if (inputY < 0) facing = "down";
             else facing = "up";
         }
     }
 
     private void handleAttack(float inputAttack)
     {
+        if (Light.CurrentCooldownTime < 0) 
+            this.gameObject.GetComponent<Light>().enabled = false;
+
         if (inputAttack == 1 && releasedAttack)
         {
-            //SoundManager.instance.RandomizeSfx(attackSound);
-            GameObject.Find("bottle").GetComponent<AudioSource>().Play();
             releasedAttack = false;
+            Debug.Log(playerNumber + " is attacking");
 
             foreach(var enemy in nearEnemy) {
-                Debug.Log("BOOM! Hit " + enemy);
+                Debug.Log(playerNumber + " is attacking " + enemy.name);
+                this.gameObject.GetComponent<Light>().enabled = true;
+                GameObject.Find("Blade").GetComponent<AudioSource>().Play();
+                
+                enemy.BroadcastMessage("damage", facing);
+                Light.CurrentCooldownTime = Light.DefaultCooldownTime;
             }
         }
         if (inputAttack == 0) releasedAttack = true;
     }
 
+    public void damage(string direction)
+    {
+        Damage.CurrentCooldownTime = Damage.DefaultCooldownTime;
+        handleDamage(direction, true);
+    }
+
+    private void handleDamage(string opponentDirection, bool damaged)
+    {
+        if (Damage.CurrentCooldownTime < 0)
+            beingDamaged = false;
+
+        if (damaged || beingDamaged)
+        {
+
+            float knockbackX = 0f, knockbackY = 0f;
+            switch (opponentDirection)
+            {
+                case ("right"): knockbackX = 20f; break;
+                case ("left"): knockbackX = -20f; break;
+                case ("down"): knockbackY = -20f; break;
+                case ("up"): knockbackY = 20f; break;
+            }
+
+            movement = new Vector2(knockbackX, knockbackY);
+            beingDamaged = true;
+        }
+    }
+
     List<GameObject> nearEnemy = new List<GameObject>();
     void OnTriggerEnter2D(Collider2D col)
     {
-/*
-        Debug.Log("Collider: " + col.gameObject.name);
-        Debug.Log("This object: " + this.gameObject.name);
-        Debug.Log(string.Compare(col.gameObject.name, this.gameObject.name));
-*/
+        var collidedObject = col.gameObject;
 
-        if (col.gameObject.tag == "Player" && string.Compare(col.gameObject.name, this.gameObject.name) > -1)
+        Debug.Log("This: " + this.gameObject.name);
+        Debug.Log("Collided Object is player: " + (collidedObject.tag == "Player"));
+
+        // Make sure we're colliding with a player, and that it is a different player than the user
+        if (collidedObject.tag == "Player" && !string.Equals(collidedObject.name, this.gameObject.name))
         {
             nearEnemy.Add(col.gameObject);
         }
